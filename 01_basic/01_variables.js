b316a33ee83f39eb6a6e890a4f9bdb2d522f52ab
063-nikhil <93570905+063-nikhil@users.noreply.github.com>
2023-11-17 21:34:42 +0000

    container config done 3

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="nikhilchawre@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2
+
+accountEmail="nikhil@gmail.com"
+accountPassword="123"
+accountCity="udaipur"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity ]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// alert(3+3);//alert not defined in node
+let name="niikhil"
+let age =43;
+let isloggedin=false;
+
+// number=>2 to power 53(apprx.)
+// bigint =>large value
+// string=>""
+// boolean=>true/false
+// null=>standalone
+// undefined=>undefined
+// Symbol=>use to identify unique components
+// Object
+
+console.log(typeof null)
+console.log(typeof undefined)
