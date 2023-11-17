88e47cf2668d0dc10589bc624a5c72da483345cd
063-nikhil <93570905+063-nikhil@users.noreply.github.com>
2023-11-17 21:56:27 +0000

    container config done 4

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,28 @@
+// let score="33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+
+
+// let score=null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+let score=undefined
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber);
