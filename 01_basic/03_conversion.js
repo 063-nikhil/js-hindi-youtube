d29a784edfdf5935ff62ca6c133c2f5a2ff57ab7
063-nikhil <93570905+063-nikhil@users.noreply.github.com>
2023-11-18 07:39:33 +0000

    lecture 7

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,28 +1,49 @@
 // let score="33abc"
-
 // console.log(typeof score)
 // console.log(typeof(score))
-
-// let valueInNumber=Number(score)
-// console.log(typeof(valueInNumber))
-// console.log(valueInNumber);
-
-
+// let valueinnumber=Number(score)
+// console.log(typeof(valueinnumber))
+// console.log(valueinnumber)
 
 // let score=null
-
 // console.log(typeof score)
 // console.log(typeof(score))
+// let valueinnumber=Number(score)
+// console.log(typeof(valueinnumber))
+// console.log(valueinnumber)
 
-// let valueInNumber=Number(score)
-// console.log(typeof(valueInNumber))
-// console.log(valueInNumber);
+// let score=undefined
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueinnumber=Number(score)
+// console.log(typeof(valueinnumber))
+//console.log(valueinnumber)
+// 
 
-let score=undefined
-
-console.log(typeof score)
-console.log(typeof(score))
-
-let valueInNumber=Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber);
+//*************** operations*****************//
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2%3)
+// console.log(2*3)
+// console.log(3**3)
+// let str1="hello"
+// let str2=" amit"
+// let str3=str1 + str2
+//console.log(str3);
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+console.log((3+4)*(5%3));//14
+console.log(+true)//1
+//console.log(true+)//error
+//console.log(+"")//0
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1,num2,num3)
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2>=1)
+// console.log(2!=1)
+// console.log(2==1)
+// console.log(2<1)
+// console.log("2">1)
+// console.log("02">1)
+//console.log(null>0)//equality check == and comparisons <><=>=work differently comparison treated null as 0
+// console.log(null==0)
+// console.log(null>=0)//
+// console.log(undefined==0)//in all threee false is displayed
+// console.log(undefined>0)
+// console.log(undefined<0)
+//strict check
+console.log("2"===2)
